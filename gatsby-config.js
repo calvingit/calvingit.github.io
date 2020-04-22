6a8fffff1bcb9ac27cfa7ef0e5e4ec213fd640eb
zhangwen <zhangwen@jobosz.com>
2020-04-22 16:17:40 +0800

    修改网站信息

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,14 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `张文的博客`,
     author: {
-      name: `Kyle Mathews`,
+      name: `张文`,
       summary: `who lives and works in San Francisco building useful things.`,
     },
-    description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
+    description: ``,
+    siteUrl: `https://calvincheung.xyz/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `calvin_cheung`,
     },
   },
   plugins: [
